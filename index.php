7cca9a4ee82e394f919e261b5ec1208f623f838f
Julien <julien1105@outlook.fr>
2018-05-01 14:28:23 +0200

    V1.3 "changement du titre de la page"

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Sequoia</title>
+  <title>Mightymmo</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
